682850f836962cd4d7796e6a3eece8ac4751bc9d
anmariyafrancis <anmariya1915@gmail.com>
2020-02-13 18:49:05 -0500

    [ANMARIYA] Created hydro subclass and init method

---
+++ CO775497_W2020_MID_TERM_MAD3004 a/CO775497_W2020_MID_TERM_MAD3004 a/Hydro.swift
@@ -0,0 +1,22 @@
+//
+//  Hydro.swift
+//  CO775497_W2020_MID_TERM_MAD3004 a
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Hydro: Bill
+{
+    var agencyName:String
+    var unitConsumed:String
+    
+    init(billId: String,billType: String,agencyName:String,unitConsumed:String)
+    {
+        self.agencyName=agencyName
+        self.unitConsumed=unitConsumed
+        super.init(billId: billId, billType: billType)
+    }
+}
