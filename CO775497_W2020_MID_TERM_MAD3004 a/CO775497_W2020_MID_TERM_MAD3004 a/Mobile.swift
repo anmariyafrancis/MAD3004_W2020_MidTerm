d03db1e5d495f11819b6bee46976d4919d2ce9cc
anmariyafrancis <anmariya1915@gmail.com>
2020-02-13 18:43:25 -0500

    [ANMARIYA] Mobile subclass created with init function

---
+++ CO775497_W2020_MID_TERM_MAD3004 a/CO775497_W2020_MID_TERM_MAD3004 a/Mobile.swift
@@ -0,0 +1,28 @@
+//
+//  Mobile.swift
+//  CO775497_W2020_MID_TERM_MAD3004 a
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Mobile: Bill
+{
+    var manufacturerName:String
+    var planName:String
+    var mobileNumber:String
+    var internetGBUsed:String
+    var minuteUsed:String
+    
+    init(billId: String,billType:String, manufacturerName:String, planName:String, mobileNumber:String, internetGBUsed:String, minuteUsed:String)
+    {
+        self.manufacturerName=manufacturerName
+        self.planName=planName
+        self.mobileNumber=mobileNumber
+        self.internetGBUsed=internetGBUsed
+        self.minuteUsed=minuteUsed
+        super.init(billId: billId, billType: billType)
+    }
+}
