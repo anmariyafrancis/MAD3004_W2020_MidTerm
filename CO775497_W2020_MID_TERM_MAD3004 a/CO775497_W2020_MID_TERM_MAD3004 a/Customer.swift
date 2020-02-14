cb87f55c1f2dc49d805dd16c76d7f7dc05ee8cd5
anmariyafrancis <anmariya1915@gmail.com>
2020-02-13 19:03:56 -0500

    [ANMARIYA] Created  customer class and init function

---
+++ CO775497_W2020_MID_TERM_MAD3004 a/CO775497_W2020_MID_TERM_MAD3004 a/Customer.swift
@@ -0,0 +1,27 @@
+//
+//  Customer.swift
+//  CO775497_W2020_MID_TERM_MAD3004 a
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Customer
+{
+    var customerId:Int
+    var firstName:String
+    var lastName:String
+    var emailId:String
+    var allCustomerBills:[String:String]
+    
+    init(customerId:Int,firstName:String,lastName:String,emailId:String,allCustomerBills:[String:String])
+    {
+        self.customerId=customerId
+        self.firstName=firstName
+        self.lastName=lastName
+        self.emailId=emailId
+        self.allCustomerBills=allCustomerBills
+    }
+}
