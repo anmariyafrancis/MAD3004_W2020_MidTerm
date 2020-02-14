1c3cd6f2037e2d6d17d0c355133b9a0931dc130a
anmariyafrancis <anmariya1915@gmail.com>
2020-02-14 18:56:17 -0500

    Created customer 2 mobile 2 object

--- CO775497_W2020_MID_TERM_MAD3004 a/CO775497_W2020_MID_TERM_MAD3004 a/main.swift
+++ CO775497_W2020_MID_TERM_MAD3004 a/CO775497_W2020_MID_TERM_MAD3004 a/main.swift
@@ -14,4 +14,5 @@ var c1i1=Internet(billId: 1, billType: "Internet", providerName: "Rogers", inter
 
 //customer 2
 var c2h1=Hydro(billId: 1, billType: "Hydro", agencyName: "Planet Energy", unitConsumed: 29)
+var c2i1=Internet(billId: 1, billType: "Internet", providerName: "Rogers", internetGBUsed: 500)
 var c2m1=Mobile(billId: 1, billType: "Mobile", manufacturerName: "Galaxy Samsung Inc.", planName:"Prepaid Talk+Text Plan", mobileNumber: 11234567890, internetGBUsed: 5, minuteUsed: 356)
