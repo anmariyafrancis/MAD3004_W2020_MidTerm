146853a0a429634aa9f9255a503d1a175cfece8d
anmariyafrancis <anmariya1915@gmail.com>
2020-02-14 19:11:52 -0500

    Created object of customer 2 and display function

--- CO775497_W2020_MID_TERM_MAD3004 a/CO775497_W2020_MID_TERM_MAD3004 a/main.swift
+++ CO775497_W2020_MID_TERM_MAD3004 a/CO775497_W2020_MID_TERM_MAD3004 a/main.swift
@@ -14,7 +14,7 @@ var c1h1=Hydro(billId: 1, billType: "Hydro", agencyName: "Planet Energy", unitCo
 var c1i1=Internet(billId: 1, billType: "Internet", providerName: "Rogers", internetGBUsed: 500)
 
 //customer 2
-
+var c2=Customer(customerId: "2", firstName: "Aldin", lastName: "Joseph", emailId: "abc@gmail.com")
 var c2h1=Hydro(billId: 1, billType: "Hydro", agencyName: "Planet Energy", unitConsumed: 29)
 var c2i1=Internet(billId: 1, billType: "Internet", providerName: "Rogers", internetGBUsed: 500)
 var c2m1=Mobile(billId: 1, billType: "Mobile", manufacturerName: "Galaxy Samsung Inc.", planName:"Prepaid Talk+Text Plan", mobileNumber: 11234567890, internetGBUsed: 5, minuteUsed: 356)
@@ -25,6 +25,7 @@ c1.display()
 c1h1.display()
 c1i1.display()
 
+c2.display()
 c2h1.display()
 c2i1.display()
 c2m1.display()
